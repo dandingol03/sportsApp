a9bcdef22d2612b9c0099e93e4b0933a9d108a90
EEvvee <609862262@qq.com>
2017-06-08 21:55:55 +0800

    ss

--- App/components/course/CustomCourse.js
+++ App/components/course/CustomCourse.js
@@ -18,6 +18,7 @@ import {connect} from 'react-redux';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import {BoxShadow} from 'react-native-shadow';
 import  Popover from  'react-native-popover'
 import CustomCourseDetail from './CustomCourseDetail';
 import Toolbar from '../../encrypt/Toolbar';
@@ -75,122 +76,145 @@ class CustomCourse extends Component{
 
     render()
     {
-
         var displayArea = {x: 5, y: 20, width:width, height: height - 25};
+        const shadowOpt = {
+            width:width-10,
+            height:126,
+            color:"#000",
+            border:2,
+            radius:3,
+            opacity:0.2,
+            x:0,
+            y:1.5,
+            style:{marginVertical:5}
+        }
 
 
         return (
             <View style={styles.container}>
                 {/*tabbar部分*/}
-
-                <Toolbar width={width} title="我的定制" actions={['发布定制']}
-                         onPress={(i)=>{
-                         console.log(i)
-                     }}
-                >
-
-                    {/*card*/}
-                    <TouchableOpacity style={{padding:4,flexDirection:'column',borderWidth:1,borderColor:'#ccc',borderStyle:'dashed',paddingBottom:7}}
+                <View style={{height:70,width:width,paddingTop:30,flexDirection:'row',justifyContent:'center',
+                    backgroundColor:'#66CDAA'}}>
+                    <TouchableOpacity style={{width:60,justifyContent:'center',alignItems: 'center',}}
+                                      onPress={()=>{this.goBack();}}>
+                        <Ionicons name={'md-arrow-back'} size={25} color="#fff"/>
+                    </TouchableOpacity>
+                    <View style={{flex:3,justifyContent:'flex-start',alignItems: 'center',flexDirection:'row'}}>
+                        <Text style={{color:'#fff',fontSize:18}}>我的定制</Text>
+                    </View>
+                    <TouchableOpacity ref="menu"  style={{flex:1,justifyContent:'center',alignItems: 'center',}}
                                       onPress={()=>{
+                          this.showPopover('menu')
+                      }}
+                    >
+                        <Ionicons name={'md-more'} size={25} color="#fff"/>
+                    </TouchableOpacity>
+                </View>
+
+                <View style={{justifyContent:'center',flexDirection:'row',padding:5,}}>
+                    <BoxShadow setting={shadowOpt}>
+                        <TouchableOpacity style={{padding:4,backgroundColor: "#fff",flexDirection:'column',paddingBottom:7}}
+                                          onPress={()=>{
                         this.navigate2MadeCustomCourseDetail()
                     }}
-                    >
-                        <View style={{flexDirection:'row',padding:2,paddingHorizontal:10,alignItems:'center'}}>
-                            <Text style={{fontSize:16,color:'#222',fontWeight:'bold'}}>
-                                壁球
-                            </Text>
-
-                            <Text style={{fontSize:14,color:'#333',marginLeft:12,fontWeight:'bold'}}>
-                                100元
-                            </Text>
-
-                            <View style={{backgroundColor:'#008B00',borderRadius:2,padding:2,marginLeft:5}}>
-                                <Text style={{color:'#fff',fontSize:11}}>
-                                    教学
+                        >
+                            <View style={{flexDirection:'row',padding:2,paddingHorizontal:10,alignItems:'center'}}>
+                                <Text style={{fontSize:16,color:'#222',fontWeight:'bold'}}>
+                                    壁球
                                 </Text>
+
+                                <Text style={{fontSize:14,color:'#333',marginLeft:12,fontWeight:'bold'}}>
+                                    100元
+                                </Text>
+
+                                <View style={{backgroundColor:'#008B00',borderRadius:2,padding:2,marginLeft:5}}>
+                                    <Text style={{color:'#fff',fontSize:11}}>
+                                        教学
+                                    </Text>
+                                </View>
+
+                                <View style={{flex:1}}></View>
+
+
+                                <Text style={{fontSize:13,color:'#008B00',fontWeight:'bold'}}>
+                                    进行中
+                                </Text>
+
                             </View>
 
-                            <View style={{flex:1}}></View>
+                            <View style={{flexDirection:'row',padding:2,paddingHorizontal:10,marginTop:10}}>
 
+                                <View style={{flexDirection:'row',alignItems:'center'}}>
+                                    <Icon name={'map-marker'} size={18} color="#666" style={{backgroundColor:'transparent'}}/>
+                                </View>
 
-                            <Text style={{fontSize:13,color:'#008B00',fontWeight:'bold'}}>
-                                进行中
-                            </Text>
+                                <View style={{flexDirection:'row',alignItems:'center',marginLeft:10}}>
+                                    <Text style={{color:'#222',fontSize:13}}>佳兴羽毛球馆</Text>
+                                </View>
 
-                        </View>
-
-                        <View style={{flexDirection:'row',padding:2,paddingHorizontal:10,marginTop:10}}>
-
-                            <View style={{flexDirection:'row',alignItems:'center'}}>
-                                <Icon name={'map-marker'} size={18} color="#666" style={{backgroundColor:'transparent'}}/>
                             </View>
 
-                            <View style={{flexDirection:'row',alignItems:'center',marginLeft:10}}>
-                                <Text style={{color:'#222',fontSize:13}}>佳兴羽毛球馆</Text>
+                            <View style={{flexDirection:'row',padding:2,paddingHorizontal:10,marginTop:10}}>
+
+                                <View style={{flexDirection:'row',alignItems:'center'}}>
+                                    <Icon name={'clock-o'} size={15} color="#666" style={{backgroundColor:'transparent'}}/>
+                                </View>
+
+                                <View style={{flexDirection:'row',alignItems:'center',marginLeft:9}}>
+                                    <Text style={{color:'#222',fontSize:13}}>2017-06-12 截止</Text>
+                                </View>
+
                             </View>
 
-                        </View>
+                            <View style={{flexDirection:'row',padding:2,paddingHorizontal:10,marginTop:10}}>
 
-                        <View style={{flexDirection:'row',padding:2,paddingHorizontal:10,marginTop:10}}>
+                                <View style={{flexDirection:'row',alignItems:'center'}}>
+                                    <Icon name={'bell-o'} size={14} color="#666" style={{backgroundColor:'transparent'}}/>
+                                </View>
 
-                            <View style={{flexDirection:'row',alignItems:'center'}}>
-                                <Icon name={'clock-o'} size={15} color="#666" style={{backgroundColor:'transparent'}}/>
+                                <View style={{flexDirection:'row',alignItems:'center',marginLeft:8}}>
+                                    <Text style={{color:'#222',fontSize:13}}>4天后到期</Text>
+                                </View>
+
                             </View>
 
-                            <View style={{flexDirection:'row',alignItems:'center',marginLeft:9}}>
-                                <Text style={{color:'#222',fontSize:13}}>2017-06-12 截止</Text>
-                            </View>
+                        </TouchableOpacity>
+                    </BoxShadow>
+                </View>
 
-                        </View>
+                <View style={{width:width,position:'absolute',bottom:20,flexDirection:'row',justifyContent:'center'}}>
 
-                        <View style={{flexDirection:'row',padding:2,paddingHorizontal:10,marginTop:10}}>
-
-                            <View style={{flexDirection:'row',alignItems:'center'}}>
-                                <Icon name={'bell-o'} size={14} color="#666" style={{backgroundColor:'transparent'}}/>
-                            </View>
-
-                            <View style={{flexDirection:'row',alignItems:'center',marginLeft:8}}>
-                                <Text style={{color:'#222',fontSize:13}}>4天后到期</Text>
-                            </View>
-
-                        </View>
-
-                    </TouchableOpacity>
-
-                    <View style={{width:width,position:'absolute',bottom:20,flexDirection:'row',justifyContent:'center'}}>
-
-                        <View style={{width:width/2,padding:6,paddingHorizontal:14,backgroundColor:'#66CDAA',
+                    <View style={{width:width/2,padding:6,paddingHorizontal:14,backgroundColor:'#66CDAA',
                            flexDirection:'row',justifyContent:'center',}}>
-                            <Text style={{color:'#fff',fontSize:15}}>
-                                发布
-                            </Text>
-                        </View>
+                        <Text style={{color:'#fff',fontSize:15}}>
+                            发布
+                        </Text>
                     </View>
+                </View>
 
 
-                    <Popover
-                        isVisible={this.state.menuVisible}
-                        fromRect={this.state.buttonRect}
-                        displayArea={displayArea}
-                        onClose={()=>{this.closePopover()
+                <Popover
+                    isVisible={this.state.menuVisible}
+                    fromRect={this.state.buttonRect}
+                    displayArea={displayArea}
+                    onClose={()=>{this.closePopover()
                         }}>
 
 
-                        <TouchableOpacity style={[styles.popoverContent]}
-                                          onPress={()=>{
+                    <TouchableOpacity style={[styles.popoverContent]}
+                                      onPress={()=>{
                               this.closePopover();
                           }}>
-                            <Text style={[styles.popoverText,{color:'#444'}]}>发布定制</Text>
-                        </TouchableOpacity>
+                        <Text style={[styles.popoverText,{color:'#444'}]}>发布定制</Text>
+                    </TouchableOpacity>
 
-                    </Popover>
-
-                </Toolbar>
+                </Popover>
 
             </View>
         )
     }
 }
+
 
 const styles = StyleSheet.create({
     container:{
