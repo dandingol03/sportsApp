b028ab7c289409dad70abd0ede50b19b8f5cc778
You Li <xqli@sdu.edu.cn>
2017-08-29 16:11:00 +0800

    xx

--- App/reducers/CompetitionReducer.js
+++ App/reducers/CompetitionReducer.js
@@ -2,29 +2,29 @@
  * Created by dingyiming on 2017/8/16.
  */
 
-import {
-    SET_COMPETATION
-} from '../constants/CompetitionConstants';
-
-const initialState = {
-    competitionList:null
-};
-
-let competations = (state = initialState, action) => {
-
-    switch (action.type) {
-
-        case  SET_COMPETATION:
-            var {competations}=action.payload
-            return Object.assign({}, state, {
-                competations:competations
-            })
-
-
-        default:
-            return state;
-    }
-}
-
-export default competations;
-
+// import {
+//     SET_COMPETATION
+// } from '../constants/CompetitionConstants';
+//
+// const initialState = {
+//     competitionList:null
+// };
+//
+// let competations = (state = initialState, action) => {
+//
+//     switch (action.type) {
+//
+//         case  SET_COMPETATION:
+//             var {competations}=action.payload
+//             return Object.assign({}, state, {
+//                 competations:competations
+//             })
+//
+//
+//         default:
+//             return state;
+//     }
+// }
+//
+// export default competations;
+//
